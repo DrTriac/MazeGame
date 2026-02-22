20989fc9f3c6868daa98acaecbccef894841ea16
thomas <thomasmenu8@gmail.com>
2026-02-22 16:21:12 +0100

    teket nu de map gecentreerd op het scherm en er wordt ook een texture toegevoegd ipv gewoon zwarte blokjes. ziet er shit uit maar maakt tis een proof of concept

---
+++ src/main/java/be/kdg/mazeGame/view/TextureManager.java
@@ -0,0 +1,23 @@
+package be.kdg.mazeGame.view;
+import javafx.scene.image.Image;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TextureManager {
+    //nieuwe hashmap aanmaken voor het stokeren van al onze image adressen en er een naam aan te kunnen linken
+    //gaat ervoor zorgan dat we in de presenter al onze textures kunnen initialiseren en ze dan gewoon kunnen gebruiken!
+
+    private static final Map<String, Image> textures = new HashMap<>();
+
+    public static Image getImage(String imagename)
+    {
+        //geef de passende image terug aan de hand van de naam die door presenter opgevraagd wordt
+        return  textures.get(imagename);
+    }
+
+    public static void loadImage(String name, String path)
+    {
+        //om alle textures op te laden, toe te voegen aan onze hash.
+        textures.put(name,new Image(TextureManager.class.getResource(path).toExternalForm()));
+    }
+}
